9f4ecef4a51a5aa825208736609c47dceb67bbf5
Thomas Wilson <tommyhwilson3@gmail.com>
2023-02-28 18:39:40 -0600

    got form working

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Let's get this party started!");
+"use strict";
+
+//attach event listener to button
+//listening for submit
+//send post request
+//append results to the DOM
+$("#search-form").on("submit", handleClick)
+
+function handleClick (evt) {
+  evt.preventDefault();
+  const searchText = $("#search-input");
+
+  console.log(searchText);
+}
+
+//console.log("Let's get this party started!");
