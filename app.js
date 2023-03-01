a00bb76fdecae837f04ef494898e9a647bc2b7b0
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-28 20:39:25 -0500

    add: url to dom (not done)

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ $("#search-form").on("submit", handleClick)
 async function handleClick (evt) {
   evt.preventDefault();
   const searchText = $("#search-input");
-  const gif = await getGif(searchText);
+  const gifs = await getGif(searchText);
+
+  const $gif = $(gifs.data.data[0]);
+  // $("#gifs").append($gif).url();
+  console.log($gif);
+  console.log($gif.attr("url"));
+  const gifUrl = $gif.attr("url");
+  $("<img></img>").attr("src", gifUrl).css("width", "100px").css("height", "auto").append("#gifs");
+  // grab a gif from gif.data.data[0]
 }
 
 async function getGif(searchText) {
   const gifResponse = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q: searchText, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}});
   return gifResponse;
 }
-
-//make ajax post request
-//need to await for response
-//console the response.data
 
 //console.log("Let's get this party started!");
