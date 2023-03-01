25984d75383e28c6f30238c4c45416a7695dc697
Thomas Wilson <tommyhwilson3@gmail.com>
2023-02-28 18:56:52 -0600

    made progress on handleClick

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ $("#search-form").on("submit", handleClick)
 function handleClick (evt) {
   evt.preventDefault();
   const searchText = $("#search-input");
+  getGif(searchText);
 
-  console.log(searchText);
+  console.log("getGif =", getGif);
 }
+
+async function getGif(searchText) {
+  const gifResponse = axios.post("http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", {searchText});
+  let response = await gifResponse
+  console.log("await response= ", response);
+}
+
+//make ajax post request
+//need to await for response
+//console the response.data
 
 //console.log("Let's get this party started!");
